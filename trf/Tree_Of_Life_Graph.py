81253171ad2142432437a2d851f3bf604294ca24
Ye Jin <49353358+rosaliejin@users.noreply.github.com>
2021-02-17 01:29:58 +0800

    Add files via upload

---
+++ trf/Tree_Of_Life_Graph.py
@@ -0,0 +1,178 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+
+get_ipython().run_line_magic('pylab', 'inline')
+import pandas as pd
+import numpy as np
+import sys
+
+
+import matplotlib
+matplotlib.rcParams['pdf.fonttype'] = 42
+matplotlib.rcParams['ps.fonttype'] = 42
+
+# Load ordered species list
+#species_list = [item.strip() for item in open("TReeOfLife_OrderedSpeciesList.txt", "r").readlines()]
+#species_order = dict(zip(species_list, range(1, len(species_list)+1)))
+
+# Load data and order by species order
+
+#data = pd.read_txt(sys.argv[1])
+data = pd.read_csv("organize.csv")
+#data["order"] = data["name"].apply(lambda x: species_order[x])
+#data = data.sort_values("order")
+
+# Remove species we don't want. e.g. Ebola
+#rmspecies = ["Ebola_virus"]
+#data = data[~data["name"].isin(rmspecies)]
+
+
+# In[8]:
+
+
+data
+
+
+# In[35]:
+
+
+# Plot repeat percentages
+
+fig = plt.figure()
+fig.set_size_inches((15, 4))
+ax = fig.add_subplot(111)
+
+bottoms = np.array([0]*data.shape[0])
+
+percolors = ["gray","red","gold","blue","green","purple"]
+
+for period in range(1, 7):
+    values = np.array(data["per%s.perc"%period])
+    ax.bar(range(len(values)), values, color=percolors[period-1], bottom=bottoms)
+    bottoms = bottoms+values
+    
+ax.set_xticks(range(len(bottoms)))
+ax.set_xticklabels(data["name"], rotation=90);
+
+ax.spines['right'].set_visible(False)
+ax.spines['top'].set_visible(False)
+ax.yaxis.set_ticks_position('left')
+ax.xaxis.set_ticks_position('bottom')
+fig.savefig("TRSpecies_PeriodPercs.pdf")
+
+
+# In[36]:
+
+
+# Plot repeat density
+
+fig = plt.figure()
+fig.set_size_inches((15, 4))
+ax = fig.add_subplot(111)
+ax.bar(range(data.shape[0]), data["str.density"], color="black", edgecolor="white");
+ax.set_xticks(range(data.shape[0]))
+ax.set_xticklabels(data["name"], rotation=90);
+fig.savefig("TRSpecies_Density.pdf")
+
+fig = plt.figure()
+fig.set_size_inches((15, 4))
+ax = fig.add_subplot(111)
+ax.bar(range(data.shape[0]), data["str.perc.bp"], color="black", edgecolor="white");
+ax.set_xticks(range(data.shape[0]))
+ax.set_xticklabels(data["name"], rotation=90);
+fig.savefig("TRSpecies_GenomePerc.pdf")
+
+
+# In[43]:
+
+
+fig = plt.figure()
+fig.set_size_inches((15, 4))
+ax = fig.add_subplot(111)
+ax.bar(range(data.shape[0]), data["homopolymers"], color="black", edgecolor="white");
+ax.set_xticks(range(data.shape[0]))
+ax.set_xticklabels(data["name"], rotation=90);
+fig.savefig("TRSpecies_homopolymer.pdf")
+
+
+# In[44]:
+
+
+fig = plt.figure()
+fig.set_size_inches((15, 4))
+ax = fig.add_subplot(111)
+ax.bar(range(data.shape[0]), data["dinucleotide"], color="black", edgecolor="white");
+ax.set_xticks(range(data.shape[0]))
+ax.set_xticklabels(data["name"], rotation=90);
+fig.savefig("TRSpecies_dinucleotide.pdf")
+
+
+# In[45]:
+
+
+fig = plt.figure()
+fig.set_size_inches((15, 4))
+ax = fig.add_subplot(111)
+ax.bar(range(data.shape[0]), data["trinucleotide"], color="black", edgecolor="white");
+ax.set_xticks(range(data.shape[0]))
+ax.set_xticklabels(data["name"], rotation=90);
+fig.savefig("TRSpecies_trinucleotide.pdf")
+
+
+# In[46]:
+
+
+fig = plt.figure()
+fig.set_size_inches((15, 4))
+ax = fig.add_subplot(111)
+ax.bar(range(data.shape[0]), data["tetranucleotide"], color="black", edgecolor="white");
+ax.set_xticks(range(data.shape[0]))
+ax.set_xticklabels(data["name"], rotation=90);
+fig.savefig("TRSpecies_tetranucleotide.pdf")
+
+
+# In[47]:
+
+
+fig = plt.figure()
+fig.set_size_inches((15, 4))
+ax = fig.add_subplot(111)
+ax.bar(range(data.shape[0]), data["pentanucleotide"], color="black", edgecolor="white");
+ax.set_xticks(range(data.shape[0]))
+ax.set_xticklabels(data["name"], rotation=90);
+fig.savefig("TRSpecies_pentanucleotide.pdf")
+
+
+# In[48]:
+
+
+fig = plt.figure()
+fig.set_size_inches((15, 4))
+ax = fig.add_subplot(111)
+ax.bar(range(data.shape[0]), data["hexanucleotide"], color="black", edgecolor="white");
+ax.set_xticks(range(data.shape[0]))
+ax.set_xticklabels(data["name"], rotation=90);
+fig.savefig("TRSpecies_hexanucleotide.pdf")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
