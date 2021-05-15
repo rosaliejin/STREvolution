d52d2cbf28a392527fae01b4ead825d94cd3f2e2
金叶 <49353358+rosaliejin@users.noreply.github.com>
2021-05-15 11:17:58 +0800

    update species list

--- trf/remove.sh
+++ trf/remove.sh
@@ -1,5 +1,5 @@
-for d in ["caeJap1","caeRem3","calJac1"]; do
-    rm -f /storage/yjin/TreeofLife/trf/{$d}/{$d}.trf.filt.bed
+for d in ["calMil1","canFam3","cavPor3","cerSim1","chlSab2","choHof1","chrPic1"]; do
+    rm -f /storage/yjin/TreeofLife/trf/$d/$d.trf.filt.bed
 done
 
 
