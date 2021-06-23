ab2712ae60032184686593a68cae08ff416fcbca
金叶 <49353358+rosaliejin@users.noreply.github.com>
2021-06-23 22:40:22 +0800

    update species order

--- trf/Tree_Of_Life_Graph.py
+++ trf/Tree_Of_Life_Graph.py
@@ -26,7 +26,7 @@ out_6 = sys.argv[10]
 
 
 # Load ordered species list
-#species_list = [item.strip() for item in open("TReeOfLife_OrderedSpeciesList.txt", "r").readlines()]
+species_list = [item.strip() for item in open("STR_SpeciesList.txt", "r").readlines()]
 #species_order = dict(zip(species_list, range(1, len(species_list)+1)))
 
 # Load data and order by species order
@@ -36,8 +36,8 @@ out_6 = sys.argv[10]
 col_name = ["name","homopolymers","dinucleotide","trinucleotide","tetranucleotide","pentanucleotide","hexanucleotide","per1.perc","per2.perc","per3.perc","per4.perc","per5.perc","per6.perc","genome_length","str_base","str.perc.bp","str.num","str.density"]
 data = pd.read_csv(infile,header = None, names = col_name)
 
-#data["order"] = data["name"].apply(lambda x: species_order[x])
-#data = data.sort_values("order")
+data["order"] = data["name"].apply(lambda x: species_order[x])
+data = data.sort_values("order")
 
 # Remove species we don't want. e.g. Ebola
 #rmspecies = ["Ebola_virus"]
